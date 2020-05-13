8096a4af145f37b92df0d116ed127b6050cbb65b
Philip Njuguna <phlpnjuguna@gmail.com>
2020-05-13 21:00:18 +0300

    fragments'

--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.fragments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addFragmentProgrammatically();
+
+    }
+    public  void addFragmentProgrammatically()
+    {
+      HelloFragment helloFragment= new HelloFragment();
+        FragmentManager manager=getFragmentManager();
+        FragmentTransaction transaction=manager.beginTransaction();
+        FragmentTransaction helloFragment1 = transaction.add(R.id.container, helloFragment, "helloFragment");
+        transaction.commit();
     }
 }
