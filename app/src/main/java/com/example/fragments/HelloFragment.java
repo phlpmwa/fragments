9277031ea582b467cb0f984f3f082da5fab264b9
Phillip Njuguna (Techno Brain Global FZE) <v-phnjug@microsoft.com>
2020-05-16 10:55:39 +0300

    fragment lifecycle

--- app/src/main/java/com/example/fragments/HelloFragment.java
+++ app/src/main/java/com/example/fragments/HelloFragment.java
@@ -2,7 +2,9 @@ package com.example.fragments;
 
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,10 +13,72 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class HelloFragment extends Fragment {
+    private  static final String TAG=HelloFragment.class.getSimpleName();
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.i(TAG,"inside onAttach ");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG,"inside onCreate ");
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.hello_fragment, container, false);
+        View view=inflater .inflate(R.layout.hello_fragment, container, false);
+        Log.i(TAG,"inside onCreateView ");
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.i(TAG,"inside onActivityCreated ");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG,"inside onStart ");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG,"inside onResume ");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG,"inside onPause ");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG,"inside onStop ");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG,"inside onDestroyView ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"inside onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.i(TAG,"inside onDetach ");
     }
 }
--- app/src/main/java/com/example/fragments/MainActivity.java
+++ app/src/main/java/com/example/fragments/MainActivity.java
@@ -3,24 +3,47 @@ package com.example.fragments;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+ public static final String TAG=MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addFragmentProgrammatically();
+        Log.e(TAG, "onCreate");
 
     }
-    public  void addFragmentProgrammatically()
-    {
-      HelloFragment helloFragment= new HelloFragment();
-        FragmentManager manager=getFragmentManager();
-        FragmentTransaction transaction=manager.beginTransaction();
-        FragmentTransaction helloFragment1 = transaction.add(R.id.container, helloFragment, "helloFragment");
-        transaction.commit();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy()");
     }
 }
